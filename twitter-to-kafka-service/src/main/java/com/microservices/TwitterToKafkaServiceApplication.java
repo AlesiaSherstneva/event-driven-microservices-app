83c333b2d08398aae476a543d05bc7b983b7a454
Alesia Sherstneva <alesiasherstneva@gmail.com>
2025-02-18 19:52:45 +0300

    Create a method for triggering the module

--- twitter-to-kafka-service/src/main/java/com/microservices/TwitterToKafkaServiceApplication.java
+++ twitter-to-kafka-service/src/main/java/com/microservices/TwitterToKafkaServiceApplication.java
@@ -1,11 +1,17 @@
 package com.microservices;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TwitterToKafkaServiceApplication {
+public class TwitterToKafkaServiceApplication implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(TwitterToKafkaServiceApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        System.out.println("The application is started");
     }
 }
