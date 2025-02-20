cdb8c4b1e3377b2a3ebcbff22e968209351d287c
Alesia Sherstneva <alesiasherstneva@gmail.com>
2025-02-20 20:47:00 +0300

    Add logging to the service and try how it works

--- twitter-to-kafka-service/src/main/java/com/microservices/TwitterToKafkaServiceApplication.java
+++ twitter-to-kafka-service/src/main/java/com/microservices/TwitterToKafkaServiceApplication.java
@@ -1,17 +1,31 @@
 package com.microservices;
 
+import com.microservices.config.TwitterToKafkaConfig;
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
+@AllArgsConstructor
 public class TwitterToKafkaServiceApplication implements CommandLineRunner {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TwitterToKafkaServiceApplication.class);
+
+    private final TwitterToKafkaConfig twitterToKafkaConfig;
+
     public static void main(String[] args) {
         SpringApplication.run(TwitterToKafkaServiceApplication.class, args);
     }
 
     @Override
     public void run(String... args) {
-        System.out.println("The application is started");
+        LOGGER.info("The application is started");
+        LOGGER.info(Arrays.toString(
+            twitterToKafkaConfig.getTwitterKeywords().toArray(new String[]{})
+        ));
     }
 }
