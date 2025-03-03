11fe1f46c3883a117ddcc53dfeb5234104d3bcc0
Alesia Sherstneva <alesiasherstneva@gmail.com>
2025-03-03 23:53:38 +0300

    Create v2 stream helper class

--- twitter-to-kafka-service/src/main/java/com/microservices/runner/helper/TwitterV2StreamHelper.java
+++ twitter-to-kafka-service/src/main/java/com/microservices/runner/helper/TwitterV2StreamHelper.java
@@ -3,9 +3,30 @@ package com.microservices.runner.helper;
 import com.microservices.config.TwitterToKafkaConfig;
 import com.microservices.listener.TwitterKafkaStatusListener;
 import lombok.RequiredArgsConstructor;
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.config.RequestConfig;
+import org.apache.hc.client5.http.cookie.CookieSpec;
+import org.apache.hc.client5.http.cookie.StandardCookieSpec;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.protocol.HttpClientContext;
+import org.apache.hc.core5.http.HttpEntity;
+import org.apache.hc.core5.http.protocol.HttpContext;
+import org.apache.hc.core5.net.URIBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import twitter4j.Status;
+import twitter4j.TwitterException;
+import twitter4j.TwitterObjectFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpResponse;
 
 @Component
 @RequiredArgsConstructor
@@ -26,7 +47,46 @@ public class TwitterV2StreamHelper {
             """;
     private static final String TWITTER_STATUS_DATE_FORMAT = "EEE MMM dd HH:mm:ss zzz yyyy";
 
-    void connectStream(String bearerToken) {
+    void connectStream(String bearerToken) throws IOException, URISyntaxException {
+        try (CloseableHttpClient httpClient = HttpClients.custom()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setCookieSpec(StandardCookieSpec.RELAXED).build())
+                .build()) {
+            URIBuilder uri = new URIBuilder(config.getTwitterV2BaseUrl());
 
+            HttpGet httpGet = new HttpGet(uri.build());
+            httpGet.setHeader("Authorization", String.format("Bearer %s", bearerToken));
+            HttpContext context = HttpClientContext.create();
+
+            CloseableHttpResponse response = httpClient.execute(httpGet, context);
+            HttpEntity entity = response.getEntity();
+
+            if (entity != null) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(entity.getContent()));
+                String nextLine = reader.readLine();
+
+                while (nextLine != null) {
+                    nextLine = reader.readLine();
+                    if(!nextLine.isEmpty()) {
+                        String tweet = getFormattedTweet(nextLine);
+
+                        Status status = null;
+                        try {
+                            status = TwitterObjectFactory.createStatus(tweet);
+                        } catch (TwitterException ex) {
+                            LOGGER.error("Could not create status for text: {}", tweet, ex);
+                        }
+
+                        if (status != null) {
+                            listener.onStatus(status);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private String getFormattedTweet(String nextLine) {
+        return null;
     }
 }
