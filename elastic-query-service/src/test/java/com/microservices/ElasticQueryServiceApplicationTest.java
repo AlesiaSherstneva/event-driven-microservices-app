a5c8bcd22618cb52bb57e4b367636a645a55635c
Alesia Sherstneva <alesiasherstneva@gmail.com>
2025-07-06 00:21:55 +0300

    Configure elastic query service

---
+++ elastic-query-service/src/test/java/com/microservices/ElasticQueryServiceApplicationTest.java
@@ -0,0 +1,11 @@
+package com.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ElasticQueryServiceApplicationTest {
+    @Test
+    public void contextLoads() {
+    }
+}
