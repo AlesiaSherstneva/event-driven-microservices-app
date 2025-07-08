94b991acab5daeedce28b86b250a25828dd39069
Alesia Sherstneva <alesiasherstneva@gmail.com>
2025-07-09 01:18:15 +0300

    Create web controller for elastic query service

--- elastic-query-service/src/main/java/com/microservices/api/ElasticDocumentController.java
+++ elastic-query-service/src/main/java/com/microservices/api/ElasticDocumentController.java
@@ -1,12 +1,40 @@
 package com.microservices.api;
 
+import com.microservices.model.ElasticQueryServiceResponseModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/documents")
 public class ElasticDocumentController {
     private static final Logger LOGGER = LoggerFactory.getLogger(ElasticDocumentController.class);
+
+    @GetMapping
+    public ResponseEntity<List<ElasticQueryServiceResponseModel>> getAllDocuments() {
+        List<ElasticQueryServiceResponseModel> response = new ArrayList<>();
+
+        LOGGER.info("Elasticsearch returned {} of documents", response.size());
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ElasticQueryServiceResponseModel> getDocumentById(@PathVariable String id) {
+        ElasticQueryServiceResponseModel response =
+                ElasticQueryServiceResponseModel.builder()
+                        .id(id)
+                        .build();
+
+        LOGGER.info("Elasticsearch returned document with id {}", id);
+
+        return ResponseEntity.ok(response);
+    }
 }
