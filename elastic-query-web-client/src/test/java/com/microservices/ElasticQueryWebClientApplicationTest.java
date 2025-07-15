c824231681be9c9a97100f3149e267048e8817d2
Alesia Sherstneva <alesiasherstneva@gmail.com>
2025-07-16 02:17:57 +0300

    Implement web client with Thymeleaf

---
+++ elastic-query-web-client/src/test/java/com/microservices/ElasticQueryWebClientApplicationTest.java
@@ -0,0 +1,11 @@
+package com.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ElasticQueryWebClientApplicationTest {
+    @Test
+    public void contextLoad() {
+    }
+}
